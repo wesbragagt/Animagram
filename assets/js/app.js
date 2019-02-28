7dec75cf3cf62dd4907576fad2228e2dd0d10cf3
Weslley Braga <wesbragagt@gmail.com>
2019-02-27 23:47:12 -0600

    created array of topics

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,11 @@
 // Jquery Document Funtions
 
+// document load function
+$(document).ready(function(){});
+
+//button click function
 $("<button>").on("click", function() {});
+
+// array of topics
+
+var topics = [];
