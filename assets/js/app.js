a4d667931f1be9103f57214050ea2b7ea06f17e7
Weslley Braga <wesbragagt@gmail.com>
2019-02-28 18:39:30 -0600

    rendered buttons to the page

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,9 +3,27 @@
 // document load function
 $(document).ready(function(){});
 
+
+
+
+
+
 //button click function
 $("<button>").on("click", function() {});
 
 // array of topics
 
-var topics = [];
+var topics = ["dog", "cat", "bird"];
+
+function renderButtons(){
+    for (var i = 0; i < topics.length; i++) {
+        var animal = topics[i];
+        var button = $("<button class = 'mr-1 btn btn-success'/>").text(animal);
+        $("#animal-button").append(button);
+        
+        
+    }
+
+}
+
+renderButtons();
