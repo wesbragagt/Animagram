cd56130dd0b3437ed410d6ea32cb44988aa889b8
Weslley Braga <wesbragagt@gmail.com>
2019-02-28 22:47:28 -0600

    added giphys to my application, need to fix some alignment and add a pause when giphys are clicked

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,58 +1,72 @@
 // Jquery Document Funtions
 
 // document load function
-$(document).ready(function(){
-    renderButtons();
-    
-   
-
-    
+$(document).ready(function() {
+  renderButtons();
 });
 
+// adds a button after submit
 $("#button-addon").on("click", function() {
-    event.preventDefault();
+  event.preventDefault();
 
-    var inputText = $("#add").val();
+  var inputText = $("#add")
+    .val()
+    .trim();
 
-    // condition to only add a new button if a value is being added
-    if(inputText !== ''){
+  // condition to only add a new button if a value is being added
+
+  if (inputText !== "") {
     topics.push(inputText);
-    $("#add").val('');
+    $("#add").val("");
     renderButtons();
-    }
-    
-    
-    
-    
-    
-    console.log(topics);
-    
-    
-    
+  }
+
+  console.log(topics);
 });
-
-
-
 
 //button click function
-
 
 // array of topics
 
 var topics = ["dog", "cat", "bird"];
 
-function renderButtons(){
+function renderButtons() {
+  $("#animal-button").empty();
 
-    $("#animal-button").empty();
-
-    for (var i = 0; i < topics.length; i++) {
-        var animal = topics[i];
-        var button = $("<button class = 'animal mr-1 btn btn-success'/>").attr("data-name", animal).text(animal);
-        $("#animal-button").append(button);
-        
-        
-    }
-
+  for (var i = 0; i < topics.length; i++) {
+    var animal = topics[i];
+    var button = $("<button class = 'animal mr-1 btn btn-success'/>")
+      .attr("data-name", animal)
+      .text(animal);
+    $("#animal-button").append(button);
+  }
 }
 
+// function which returns the name of the animal after clicked
+function alertAnimal() {
+  var animalName = $(this).attr("data-name");
+  alert(animalName);
+}
+$(document).on("click", ".animal", getApi);
 
+// setup the ajax call for the giffy api
+function getApi() {
+  var animal = $(this).attr("data-name");
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + animal;
+
+  console.log(queryURL);
+
+  // ajax request
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    var imageURL = response.data.images.original.url;
+    console.log(imageURL);
+
+    var animalImg = $("<img class = 'animalImg m-2'>");
+    animalImg.attr({ src: imageURL, alt: animal });
+    $("#imageHere").prepend(animalImg);
+  });
+}
